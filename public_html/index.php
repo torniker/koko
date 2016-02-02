6cb5abde428c88ee1369bac0d2e4a36639771599
Tornike Razmadze <torniker@gmail.com>
2016-02-02 12:12:41 +0400

    testing git deploy

--- public_html/index.php
+++ public_html/index.php
@@ -6,18 +6,17 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
-die('sadfsda');
-// /*
-// |--------------------------------------------------------------------------
-// | Register The Auto Loader
-// |--------------------------------------------------------------------------
-// |
-// | Composer provides a convenient, automatically generated class loader for
-// | our application. We just need to utilize it! We'll simply require it
-// | into the script here so that we don't have to worry about manual
-// | loading any of our classes later on. It feels nice to relax.
-// |
-//  */
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+ */
 
 require __DIR__ . '/../bootstrap/autoload.php';
 

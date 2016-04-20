2e4adb2931dc0492d58d56ece0fc4849921fcb1f
Tornike Razmadze <torniker@gmail.com>
2016-04-20 12:58:10 +0400

    started writing note about machine learning

---
+++ resources/views/notes/machine-learning-overview.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('title', 'Machine Learning Overview')
+
+@section('content')
+    <div>
+        <div class="jumbotron">
+            <div class="container text-center">
+                <h1>
+                    Machine Learning
+                </h1>
+                <p>
+                Recently I've started (again) a course of <a href="https://www.coursera.org/learn/machine-learning/" target="_blank">Machine Learning on Coursera.</a>
+                These are some notes about the course.
+                </p>
+            </div>
+        </div>
+        <div class="content container">
+
+        </div>
+    </div>
+@endsection
